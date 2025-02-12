93b49a4866e6c16a9d9db18d5ff436c47267cd88
Matthew Miner <mminer237@gmail.com>
2025-02-12 17:57:58 -0600

    Update IPA Reader domain name

--- ipa-pronunciation-linker.user.js
+++ ipa-pronunciation-linker.user.js
@@ -2,7 +2,7 @@
 // @name         IPA Pronunciation Linker
 // @description  Automatically link IPA pronunciations to a tool to pronounce them
 // @author       Matthew Miner
-// @version      1.0
+// @version      1.1
 // @namespace    https://matthewminer.name
 // @copyright    2022, Matthew Miner (https://matthewminer.name)
 // @license      MIT
@@ -35,7 +35,7 @@
 			text = text.replace(/u[\u0300-\u031c\u031f-\u036f]/gu, 'u');
 
 			/* Insert link */
-			span.innerHTML = `<a href="http://ipa-reader.xyz/?text=${text}&voice=${voice}" target="_blank">${span.innerText}</a>`;
+			span.innerHTML = `<a href="https://ipa-reader.com/?text=${text}&voice=${voice}" target="_blank">${span.innerText}</a>`;
 		}
 	});
 })();
